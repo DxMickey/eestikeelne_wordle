15abca0d3f4ad3a1987084afe8e1f9229d536915
JVaht-98 <jaagup.vaht@gmail.com>
2022-04-29 00:23:15 +0300

    achievemnts komponendi algus

--- AchievementChecker/CAchievements.vb
+++ AchievementChecker/CAchievements.vb
@@ -1,12 +1,22 @@
 ﻿Public Class CAchievements
     Implements IAchievments
 
-    'Otsib andmebaasist saavutused
     Public Function getAchievements() As Object Implements IAchievments.getAchievements
+        'Otsib andmebaasist eri andmeid eri saavutuste kohta (vt funktsioone)
+        'tagastab stringide massiivi, mida saab notificationinga näidata
+        Dim data As Andmekiht.IDatabase
+        data = New Andmekiht.CDatabase
+        Dim gamesPlayed = data
+
+    End Function
+
+    Public Function setAchievements() As Object Implements IAchievments.setAchievements
+        'vaata mis achievmente saadi. Lisa andmebaasi,kui midagi uut
         Throw New NotImplementedException()
     End Function
-    'Otsib uusi saavutusi andmebaasist saadud andmetega
-    Public Function setAchievements() As Object Implements IAchievments.setAchievements
-        Throw New NotImplementedException()
+    Private Function getGamesPlayed(db As Andmekiht.CDatabase)
+        Dim nrOfGamesPlayed As Integer
+        'get games played from db
+        Return nrOfGamesPlayed
     End Function
 End Class
